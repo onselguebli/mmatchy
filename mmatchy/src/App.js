cf25928d8bc0e88a23efb17c1f019ea70ec1f98c
onselguebli <onsgueblii@gmail.com>
2022-11-21 23:27:11 +0100

    some init msg

---
+++ mmatchy/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from './img/Sanstitre.jpg'
+function App() {
+  return (
+    <div className="App">
+     <h1>hello everyone </h1>
+     <h2>yeaaaahhh</h2>
+     <img srec={image} />
+    </div>
+  );
+}
+
+export default App;
